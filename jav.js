8e90005a04ed3dac59e2b62dea0f39505284547c
Nayab <157101214+Nayabsaleem@users.noreply.github.com>
2024-10-28 23:30:24 +0500

    Add files via upload

---
+++ jav.js
@@ -0,0 +1,215 @@
+
+// Assignment #1 ch:1
+//Q: 1
+// alert ("Welcome to Learning javaScript");
+//Q: 2
+ //alert (" Error! Please enter a valid password");
+//Q: 3
+ //alert ("Welcome to JS land..... \n Happy coding");
+//Q: 4
+//alert ("Welcome to JS Land...");
+ //alert("Happy Coding! \n Prevent this page from creating additional dialogs");
+// Q:5
+ //alert("Hello.... I can run JS through Web browser's Console");
+// Q:6
+// alert("hii !!!");
+
+
+
+// Assignment #2 ch:2
+//Q:1
+// alert
+// var userName = "Hello Welcome to coding";
+// alert(userName);
+
+//Q:2
+// var myName = "NayabKhan";
+// alert(myName);
+//Q:3
+//   var msg= "Hello World!!!";
+//    alert(msg);
+// Q:4
+// var name = "NAYAB" ;  
+// var age = 22;
+// var coursetitle = "Certified Mobile Application Development"
+// alert(name);
+// alert(age);
+// alert(coursetitle);
+//Q :5
+// var p ="Pizza \n Pizz \n Piz \n Pi \n P"
+// alert(p)
+//Q :6
+// var email = "nayabsaleem280@gmail.com";
+// alert("my email address is  "   + email);
+//Q:7
+// var book = "A Smarter way to learn JavaScript";
+// alert("I am trying to learn from the book " + book);
+// Q :8
+// var logo = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(logo);
+
+
+
+//Assignment #3  ch:3
+//Q:1
+// var age = "22 year old";
+// alert("My age is "+ age );
+// Q:2
+// var web = prompt("How many times have you visit webpage?"); 
+// alert(web + "times" );
+//Q:3
+// var birthyear = "1947";
+// document.write("My birth year is " + birthyear);
+// document.write("Data type of my variable is" + birthyear +"<br>");
+
+//Q:4
+// var visitor = prompt("Enter your name");
+// var title = prompt("Enter your product title");
+// var order = prompt("How many visitor you want to order?");
+// document.write(visitor+ "order " + order + "t-shirt to" + title);
+
+
+
+//Assignment :4 ch:4
+// Q:1
+// let name;
+// let Father;
+// let RollNo;
+
+// Q:2
+// let myName;
+// let fName;
+// let myVar;
+// let _MYname;
+// let $f_name;
+// let my_Name;
+// let name123;
+
+//Q:3
+// document.write(" <h1>Rules for Naming Js Variable</h1> <br>");
+// document.write("Variable must begin letter _ , $ , and myNmae123 <br>");
+// document.write("Variables must begin with a letter , $  or _ For example $name, _name or $name <br>");
+// document.write("Variable names are case sensitive <br>");
+// document.write(" Variable names should not be JS keywords <br>");
+
+
+
+
+// Assignment:5 ch:5
+
+//Q:1
+// let x = 4;
+//     y = 6;
+//  result = x+y;
+//  document.write("Sum of two number is " + result + "<br>");
+
+ //Q:2
+//  let  a = 54;
+//       b = 32;
+//  document.write("Multiply of two number is " + a*b + "<br>" );
+//  document.write("Divide of two number is " + a/b  + "<br>");
+//  document.write("Modulas of two number is " + a%b  + "<br>");
+
+ //Q:3
+//  let value;
+// value="value after variable declartion is undefined";
+// console.log(value + "\n");
+// console.log("initial value :5");
+// console.log("value after increament is :6");
+// console.log("value after addition is :13");
+// console.log("value after decrement is:12");
+// console.log("The reminder is:0");
+
+//Q:4
+// let ticketprice = 600;
+// let purchaseticket = ticketprice*5;
+// console.log("Total cost to buy a new tickets  to a movie is" + purchaseticket);
+
+//Q:5
+// let  table = 7;
+// for (i = 1; i<=10; i ++){
+//      document.write(table + "*" +i+"="+table*i +"<br>")
+// }
+
+//Q:6
+// let celsius=34;
+// let fahrenheit= (34*9/5)+32;
+// console.log("34 celsius is "+ fahrenheit +"F");
+ 
+// let fahrenheit_2=50;
+// let celsius_2=(50-32)*5/9;
+// console.log("50 fahrenheit is"+ celsius_2 +"C");
+
+//Q:7
+// let priceitem1 =650;
+// let quatity=3;
+// let priceitem2 =100;
+// let quantity2=7;
+// let shippingcharges=100;
+// let total_cost = priceitem1* quatity + priceitem2 * quantity2 +shippingcharges
+
+// console.log("price of item 1 is "+ priceitem1 );
+// console.log("quantity of item 1 is " + quatity);
+// console.log("price of item 2 is 100" + priceitem2);
+// console.log("quantity of item 2 is 7" + quantity2);
+// console.log("shipping charges is " + shippingcharges);
+// console.log("Total cost of your order is "+ total_cost);
+
+//Q:8
+// let marks=980;
+// console.log("Total marks:"+ marks);
+// let marksobtained=804;
+// console.log("marks obtained:"+ marksobtained);
+// let percentage=(marksobtained/marks*100);
+// console.log("percentage:"+percentage);
+
+//Q:9
+// let lUSdollar = 10;
+// let Riyal = 25;
+// let USdollar_1=104.80;
+// let riyals_1=28 ;
+// let totalcurrency = USdollar*USdollar_1 + Riyal*riyals_1
+// console.log("Total currency in Pkr is "+totalcurrency);
+
+//Q:10
+// let number=25;
+// console.log(number+5 + " addition");
+// console.log(number*10 + " multiply");
+// console.log(number/2 + " division");
+
+//Q:11
+// let currentyear = 2024;
+// let birthyear = 2000;
+// let agethisyear = currentyear - birthyear;
+// let agelastyear = agethisyear -1;
+// console.log("They are either "+ agethisyear + " or" + agelastyear + " years old") ;
+
+
+//Q:12
+// let radius=30;
+// console.log("Radius of a circle is " + radius);
+// let circumference = (2*3.142*radius);
+// console.log("the circumference of a circle is " + circumference);
+// let area = (3.142*radius*2);
+// console.log("The area is "+ area);
+
+//Q:13
+// let favouritesnack = "kitkat";
+// console.log("favourite snack: "+favouritesnack);
+// let currentage = 22;
+// console.log("currentage: "+ currentage)
+// let maximumage = 55;
+// console.log("Estimated maxiumum age:"+ maximumage);
+// let amountage = 2;
+// console.log("Amount of snack per day:"+ amountage);
+// console.log("you will need "+(maximumage-currentage)*amountage+ " kitkat to last you until the ripe old age of " +maximumage);
+
+
+
+
+
+
+
+
+
+
